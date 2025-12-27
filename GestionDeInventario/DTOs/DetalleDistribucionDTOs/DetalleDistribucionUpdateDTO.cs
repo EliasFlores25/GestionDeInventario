f50e3109025e2dfa84e1fb9f6899a91abf427270
StefaniePaiz <ps25001@esfe.agape.edu.sv>
2025-12-26 19:18:05 -0600

    Se termino de crear

--- GestionDeInventario/DTOs/DetalleDistribucionDTOs/DetalleDistribucionUpdateDTO.cs
+++ GestionDeInventario/DTOs/DetalleDistribucionDTOs/DetalleDistribucionUpdateDTO.cs
@@ -2,5 +2,17 @@
 {
     public class DetalleDistribucionUpdateDTO
     {
+        public int IdDetalleDistribucion { get; set; }
+
+        public int IdUsuario { get; set; }
+        public int IdEmpleado { get; set; }
+        public int IdProducto { get; set; }
+
+        public int Cantidad { get; set; }
+        public DateTime FechaSalida { get; set; }
+        public string Motivo { get; set; }
+
+        public decimal PrecioCostoUnitario { get; set; }
+        public decimal MontoTotal { get; set; }
     }
 }
