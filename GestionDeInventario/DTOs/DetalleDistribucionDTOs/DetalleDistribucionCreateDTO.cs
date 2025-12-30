81d4ef6b3f810dda281683068c32bbca06b9f376
StefaniePaiz <ps25001@esfe.agape.edu.sv>
2025-12-30 14:58:26 -0600

    Se siguio trabajando en el los DTOS

--- GestionDeInventario/DTOs/DetalleDistribucionDTOs/DetalleDistribucionCreateDTO.cs
+++ GestionDeInventario/DTOs/DetalleDistribucionDTOs/DetalleDistribucionCreateDTO.cs
@@ -1,15 +1,32 @@
-﻿using GestionDeInventario.Models;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using GestionDeInventario.Models;
 
 namespace GestionDeInventario.DTOs.DetalleDistribucionDTOs
 {
     public class DetalleDistribucionCreateDTO
     {
+        [Required(ErrorMessage = "El número de distribución es obligatorio")]
+        [StringLength(50, ErrorMessage = "El número de distribución no puede exceder 50 caracteres")]
         public string NumeroDistribucion { get; set; }
+
+        [Required(ErrorMessage = "El usuario es obligatorio")]
         public int UsuarioId { get; set; }
+
+        [Required(ErrorMessage = "El empleado es obligatorio")]
         public int EmpleadoId { get; set; }
+
+        [Required(ErrorMessage = "El producto es obligatorio")]
         public int ProductoId { get; set; }
+
+        [Required(ErrorMessage = "La cantidad es obligatoria")]
+        [Range(1, int.MaxValue, ErrorMessage = "La cantidad debe ser mayor a 0")]
         public int Cantidad { get; set; }
+
+        [Required(ErrorMessage = "La fecha de salida es obligatoria")]
         public DateTime FechaSalida { get; set; }
+
+        [StringLength(200, ErrorMessage = "El motivo no puede exceder 200 caracteres")]
         public string? Motivo { get; set; }
     }
 }
--- GestionDeInventario/DTOs/DetalleDistribucionDTOs/DetalleDistribucionResponseDTO.cs
+++ GestionDeInventario/DTOs/DetalleDistribucionDTOs/DetalleDistribucionResponseDTO.cs
@@ -1,24 +1,48 @@
-﻿using GestionDeInventario.Models;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using GestionDeInventario.Models;
 
 namespace GestionDeInventario.DTOs.DetalleDistribucionDTOs
 {
     public class DetalleDistribucionResponseDTO
     {
+        [Required]
         public int IdDetalleDistribucion { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string NumeroDistribucion { get; set; }
-        public Usuario Usuario { get; set; }
+
+        [Required]
         public int UsuarioId { get; set; }
-        public Empleado Empleado { get; set; }
+
+        public Usuario Usuario { get; set; }
+
+        [Required]
         public int EmpleadoId { get; set; }
-        public Producto Producto { get; set; }
+
+        public Empleado Empleado { get; set; }
+
+        [Required]
         public int ProductoId { get; set; }
 
+        public Producto Producto { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue)]
         public int Cantidad { get; set; }
+
+        [Required]
         public DateTime FechaSalida { get; set; }
+
+        [StringLength(200)]
         public string Motivo { get; set; }
 
+        [Required]
+        [Range(0.01, double.MaxValue)]
         public decimal PrecioCostoUnitario { get; set; }
-        public decimal MontoTotal { get; set; }
 
+        [Required]
+        public decimal MontoTotal { get; set; }
     }
 }
